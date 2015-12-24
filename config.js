5fa5fc4c190f81acbce1c4d32d0a058b72402cfe
Tugberk Ugurlu <tugberk@outlook.com>
2015-12-24 10:12:37 +0000

    minimum working piece

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ System.config({
   defaultJSExtensions: true,
   transpiler: "typescript",
   paths: {
+    "*": "dist/*",
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
